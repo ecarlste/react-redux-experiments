95806f99094c4d31a44fe7266216840b95788cdd
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-09 01:22:13 -0700

    Improved grid implementation

--- pics/src/components/ImageList/ImageCard/ImageCard.js
+++ pics/src/components/ImageList/ImageCard/ImageCard.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
 class ImageCard extends Component {
+  imageRef = React.createRef();
+  state = { spans: 0 };
+
   render() {
+    const { urls, alt_description } = this.props.image;
+
     return (
-      <div>
-        <img src={this.props.image.urls.regular} alt={this.props.image.alt_description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={alt_description} />
       </div>
     );
   }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans: spans });
+  };
 }
 
 export default ImageCard;
