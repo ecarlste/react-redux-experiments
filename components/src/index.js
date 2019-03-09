dc98dffe2b9ba33bdb37bee4668f191bd06793d6
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-08 17:01:11 -0700

    Used semanticui to built a comment in jsx

---
+++ components/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a class="avatar" href="/">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a className="author" href="/">{faker.name.firstName()}</a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>  
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
