fc6e56aede233ec4d03799a7959cf01dde6e7878
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-08 22:52:22 -0700

    Refactored SearchBar to be a controlled component

--- pics/src/components/SearchBar/SearchBar.js
+++ pics/src/components/SearchBar/SearchBar.js
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
-import { Segment, Form } from "semantic-ui-react";
+import React, { Component } from 'react';
+import { Segment, Form } from 'semantic-ui-react';
 
 class SearchBar extends Component {
+  state = { term: '' };
+
   render() {
     return (
       <Segment>
         <Form>
           <Form.Field>
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })} />
           </Form.Field>
         </Form>
       </Segment>
