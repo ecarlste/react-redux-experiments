9a9959f6eeb2c6a49c1498e9f85129e4905d33fc
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-08 19:34:28 -0700

    Small refactor to remove constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,27 +2,25 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
 
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>loading...</div>;
+  }
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
-  }
-
-  render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-    }
-
-    if (this.state.lat && !this.state.errorMessage) {
-      return <div>Latitude: {this.state.lat}</div>
-    }
-
-    return <div>loading...</div>
   }
 }
 
