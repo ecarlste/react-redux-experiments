2792c3db2f9c8691581eaf8e452b8c83b56b002e
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-08 18:33:34 -0700

    Initial boilerplate commit for seasons app project

---
+++ seasons/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>here is some text</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
