5aa7fc78466b85cf135802563ae60f919cfcd19a
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-10 15:31:20 -0600

    Created initial actions and reducers

---
+++ songs/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'title 1', duration: '1:00' },
+    { title: 'title 2', duration: '2:00' },
+    { title: 'title 3', duration: '3:00' },
+    { title: 'title 4', duration: '4:00' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return action.payload;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
