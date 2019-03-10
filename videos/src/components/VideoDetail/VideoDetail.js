0f57896f12aa5114385662eebf293217196b1b4d
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-10 13:25:54 -0600

    Created a video player embed for playing the selected video

--- videos/src/components/VideoDetail/VideoDetail.js
+++ videos/src/components/VideoDetail/VideoDetail.js
@@ -8,8 +8,13 @@ export default class VideoDetail extends Component {
       return <div>No video selected...</div>;
     }
 
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
       <div>
+        <div className="ui embed">
+          <iframe src={videoSrc} title="Video Player" />
+        </div>
         <div className="ui segment">
           <h4 className="ui header">{video.snippet.title}</h4>
           <p>{video.snippet.description}</p>
