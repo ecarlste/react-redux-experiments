96af82a505c4e6fd44435b5c4251b37e9e8f5a69
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-10 22:59:30 -0600

    Changed user reducer to replace duplicates by ID on insert

--- blog/src/reducers/userReducer.js
+++ blog/src/reducers/userReducer.js
@@ -1,17 +1,15 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 
-export default (state = [], action) => {
-  switch (action.type) {
+export default (state = [], { type, payload: user }) => {
+  switch (type) {
     case 'FETCH_USER':
-      // const existingUser = _.find(state, { id: user.id });
+      const existingUser = _.find(state, { id: user.id });
 
-      // if (existingUser) {
-      //   return [...state.filter(u => u.id !== user.id), ...user];
-      // } else {
-      //   return [...state, ...user];
-      // }
-
-      return [...state, action.payload];
+      if (existingUser) {
+        return [...state.filter(u => u.id !== user.id), user];
+      } else {
+        return [...state, user];
+      }
 
     default:
       return state;
