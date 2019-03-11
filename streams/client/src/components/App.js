cebe10b8d793c70fe74af7f9888e21e2ad20b8ba
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-11 10:29:52 -0600

    Created header component for top nav

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,17 +1,19 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <Router>
           <Fragment>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/create" component={StreamCreate} />
             <Route path="/streams/show" component={StreamShow} />
