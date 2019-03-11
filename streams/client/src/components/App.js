ac0d29310d27a1e77e0f580ff61beaad70710bb7
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-11 00:53:52 -0600

    Created streams components and routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,22 +1,10 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
-class PageOne extends Component {
-  render() {
-    return <div>PageOne</div>;
-  }
-}
-
-class PageTwo extends Component {
-  render() {
-    return (
-      <div>
-        PageTwo
-        <button>Click Me!</button>
-      </div>
-    );
-  }
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 export default class App extends Component {
   render() {
@@ -24,8 +12,11 @@ export default class App extends Component {
       <div>
         <Router>
           <Fragment>
-            <Route path="/" exact component={PageOne} />
-            <Route path="/pagetwo" component={PageTwo} />
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/create" component={StreamCreate} />
+            <Route path="/streams/show" component={StreamShow} />
+            <Route path="/streams/edit" component={StreamEdit} />
+            <Route path="/streams/delete" component={StreamDelete} />
           </Fragment>
         </Router>
       </div>
