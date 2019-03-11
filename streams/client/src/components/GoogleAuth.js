da2b186150ab8e03592debde95f12b02c5e1eb73
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-11 17:03:34 -0600

    Set up redux boilerplate

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -12,14 +12,14 @@ export default class GoogleAuth extends Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="icon google" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button onClick={this.onSignIn} className="ui red google button">
+        <button onClick={this.onSignInClick} className="ui red google button">
           <i className="icon google" />
           Sign In with Google
         </button>
@@ -28,15 +28,14 @@ export default class GoogleAuth extends Component {
   }
 
   onAuthChange = () => {
-    console.log('env', process.env);
     this.setState({ isSignedIn: Boolean(window.firebase.auth().currentUser) });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     window.firebase.auth().signInWithPopup(new window.firebase.auth.GoogleAuthProvider().addScope('email'));
   };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     window.firebase.auth().signOut();
   };
 
