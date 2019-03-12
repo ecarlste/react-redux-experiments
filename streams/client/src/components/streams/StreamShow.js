ec8e8c157b17bf786f0113c8fb7183760c4dc23d
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 14:13:03 -0600

    Added basic video player to streamshow

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 export class StreamShow extends Component {
+  videoRef = React.createRef();
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
@@ -12,6 +14,7 @@ export class StreamShow extends Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
