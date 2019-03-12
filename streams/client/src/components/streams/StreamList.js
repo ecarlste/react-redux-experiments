d23fe2059c2158ed8b0bd0e64bc247e301c4d318
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-11 22:31:03 -0600

    Added edit/delete buttons for owner of stream

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -12,10 +12,11 @@ export class StreamList extends Component {
     );
   }
 
-  renderList() {
+  renderList = () => {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large icon middle aligned camera" />
           <div className="content">
             {stream.title}
@@ -24,7 +25,18 @@ export class StreamList extends Component {
         </div>
       );
     });
-  }
+  };
+
+  renderAdmin = stream => {
+    if (this.props.currentUserId === stream.userId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  };
 
   componentDidMount = () => {
     this.props.fetchStreams();
@@ -33,7 +45,8 @@ export class StreamList extends Component {
 
 const mapStateToProps = state => {
   return {
-    streams: Object.values(state.streams)
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
   };
 };
 
