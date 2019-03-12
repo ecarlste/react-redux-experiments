a6b86f0b58c2949aaca2660b30d3f0ba27aad068
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 11:20:52 -0600

    Built out first attempt at react modal

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import Modal from '../Modal';
 
 export default class StreamDelete extends Component {
   render() {
-    return <div>StreamDelete</div>;
+    return (
+      <div>
+        StreamDelete
+        <Modal />
+      </div>
+    );
   }
 }
