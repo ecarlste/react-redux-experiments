d23f6647b96638febbbb49970de31acc4615feb2
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 17:17:00 -0600

    Extracted view logic for language selection

---
+++ translate/src/components/LanguageSelector.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class LanguageSelector extends Component {
+  render() {
+    return (
+      <div>
+        Select a Language:
+        <i className="flag us" onClick={() => this.props.onLanguageChange('english')} />
+        <i className="flag nl" onClick={() => this.props.onLanguageChange('dutch')} />
+      </div>
+    );
+  }
+}
