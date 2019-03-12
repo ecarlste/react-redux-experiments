f57ced7f20443165c2e4d1b34d46ff478bbbd13b
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 16:55:19 -0600

    Removed superfluous static context declare

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 export default class Button extends Component {
-  static contextType = LanguageContext;
-
   static config = {
     english: {
       submitText: 'Submit'
