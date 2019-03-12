187ab940f03ec9f5d8615e18964eefeb1cde8b8b
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 13:14:07 -0600

    Implemented initial redux setup for streamshow

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-export default class StreamShow extends Component {
+export class StreamShow extends Component {
   render() {
-    return <div>StreamShow</div>;
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
   }
+
+  componentDidMount = () => {
+    this.props.fetchStream(this.props.match.params.id);
+  };
 }
+
+const mapStateToProps = (state, ownProps) => ({
+  stream: state.streams[ownProps.match.params.id]
+});
+
+const mapDispatchToProps = {
+  fetchStream
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(StreamShow);
