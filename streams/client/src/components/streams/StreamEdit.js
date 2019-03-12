3d93bdec7e01b13c976aaf8b9df18d136396a927
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 00:07:03 -0600

    Initial version of stream edit page

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-export default class StreamEdit extends Component {
+export class StreamEdit extends Component {
   render() {
-    return <div>StreamEdit</div>;
+    const { stream } = this.props;
+    const { id } = this.props.match.params;
+
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        Stream Id: {id}
+        <br />
+        Stream Title: {this.props.stream.title}
+      </div>
+    );
   }
+
+  componentDidMount = () => {
+    this.props.fetchStream(this.props.match.params.id);
+  };
 }
+
+const mapStateToProps = (state, ownProps) => {
+  console.log(state.streams);
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+const mapDispatchToProps = {
+  fetchStream
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(StreamEdit);
