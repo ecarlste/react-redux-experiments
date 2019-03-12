886b88147ec29930b39bd635c80b881d492b15ea
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 15:33:06 -0600

    Added language selection icons

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = { language: 'english' };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a Language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
 }
 
 export default App;
