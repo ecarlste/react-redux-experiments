470436e068da2376ca778f7acf7a0602fab7251d
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 20:07:11 -0600

    Set up ResourceList to fetch data from api

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ResourceList extends Component {
+  state = { resources: [] };
+
   render() {
-    return <div>{this.props.resource}</div>;
+    return <div>{this.state.resources.length}</div>;
+  }
+
+  componentDidMount = async () => {
+    this.setStateWithFetchedResources(this.props.resource);
+  };
+
+  componentDidUpdate = async prevProps => {
+    if (prevProps.resource !== this.props.resource) {
+      this.setStateWithFetchedResources(this.props.resource);
+    }
+  };
+
+  async setStateWithFetchedResources(resourceType) {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resourceType}`);
+    this.setState({ resources: response.data });
   }
 }
