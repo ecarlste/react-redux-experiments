d2828581136c45b790a8857cafec3247b1b2ea12
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 22:03:36 -0600

    Extracted useLocation into a hook

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,18 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Loader from './components/Loader/Loader';
 import SeasonDisplay from './components/SeasonDisplay/SeasonDisplay';
+import useLocation from './components/useLocation';
 
 const App = () => {
-  const [lat, setLat] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      position => setLat(position.coords.latitude),
-      err => setErrorMessage(err.message)
-    );
-  }, []);
+  const [lat, errorMessage] = useLocation();
 
   let content;
 
