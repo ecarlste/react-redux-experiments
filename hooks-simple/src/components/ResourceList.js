4aea239d5d39f0149a69ffce766a57dd13a0e78a
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-12 21:04:33 -0600

    Extracted useResources axios logic to be reusable

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,18 +1,8 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import useResources from './useResources';
 
 const ResourceList = ({ resource }) => {
-  const [resources, setResources] = useState([]);
-
-  const fetchResource = async resource => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-
-    setResources(response.data);
-  };
-
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource]);
+  const resources = useResources(resource);
 
   return (
     <div>
